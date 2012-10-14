3af03433b8f9b70cc084ad10f976ac6f858f4bd9
Shaun Scaling <shaun@Shaun.local>
2012-10-14 11:30:10 +0100

    Initial Commit

---
+++ jsonprint/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  jsonprint
+//
+//  Created by Shaun Scaling on 14/10/2012.
+//  Copyright (c) 2012 Shaun Scaling. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
