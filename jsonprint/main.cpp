f9a2c231d10dbc21f17fd98fadccebf2bcf8c028
Shaun Scaling <shaun@Shaun.local>
2012-10-15 00:36:47 +0100

    20% working parser. v basic.

--- jsonprint/main.cpp
+++ jsonprint/main.cpp
@@ -7,12 +7,51 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <unistd.h>
+
+#include "Json.h"
+
+const char const * kTestFile = "/Users/shaun/Development/jsonprint/jsonprint/test.json";
 
 int main(int argc, const char * argv[])
 {
+    using std::cout;
+    using std::ifstream;
+    using std::endl;
+    using std::ios;
+    
+    //
+    // get pwd
+    char buff[256];
+    getcwd( buff, 256 );
+    cout << "Starting in '" << buff << "'" << endl;
+    
+    ifstream::pos_type size = 0;
+    char * jsonStr;
+    
+    //
+    // read in test json
+    ifstream file(kTestFile, ios::in|ios::ate);
+    if ( file.is_open() )
+    {
+        size = file.tellg();
+        jsonStr = new char [size];
+        file.seekg (0, ios::beg);
+        file.read (jsonStr, size);
+        file.close();
+    } else {
+        cout << "Unabled to open '" << kTestFile << "'" << endl;
+    }
 
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    Json* j = new Json(jsonStr, (int)size);
+    
+    cout << "Json input has '" << (j->isValid()?j->toString():"") << "'" << endl;
+    
+    delete [] jsonStr;
+    
+    
+    cout << "exiting normally";
+    
     return 0;
 }
-
